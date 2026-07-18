ce6cf9e11538aa898e5efd0bee7f762c12eed64e
Harshini <harshini4126@gmail.com>
2026-07-18 14:59:07 +0530

    Task:1

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,17 @@
+# print ("hello")
+# num_char = len("hello")
+# print(num_char)
+
+
+#
+# def my_function():
+#     print("hello")
+#     print("bye")
+# my_function()
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+my_function()
