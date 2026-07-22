50177969c99ed2fc22272dba74d4da7907b825a3
Harshini <harshini4126@gmail.com>
2026-07-22 15:19:07 +0530

    Task_1

---
+++ Day 10/Functions with Outputs/task.py
@@ -0,0 +1,31 @@
+# def format_name(f_name, l_name):
+#     print(f_name.title())
+#     print(l_name.title())
+# format_name("sonu","city")
+#
+# def function_1(text):
+#     return text + text
+# def function_2(text):
+#     return text.title()
+# output = function_1("France")
+# print(output)
+
+def format_name(f_name, l_name):
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+
+print(format_name("AnGeLa", "YU"))
+
+
+def function_1(text):
+    return text + text
+
+
+def function_2(text):
+    return text.title()
+
+
+output = function_2(function_1("hello"))
+print(output)
