e2baedcf7d7930f23b6e7d2d1f5e72dfa730f063
Harshini <harshini4126@gmail.com>
2026-08-01 12:40:34 +0530

    Clean repo to restart from Day 1

---
+++ Day 01/Band Name Generator Project/task/task.py
@@ -0,0 +1,9 @@
+print("Welcome to thr Band Name Generator")
+city = input("What's the name of the city you grew up?")
+print(city)
+pet = input("What's your pet name?")
+print(pet)
+print("your band name could be:" +city +" "+pet)
+
+
+
