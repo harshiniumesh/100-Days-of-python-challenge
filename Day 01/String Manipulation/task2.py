cbfc1a4be5592231067065e56ee9aed38740db88
Harshini <harshini4126@gmail.com>
2026-08-01 14:06:38 +0530

    tasks

---
+++ Day 01/String Manipulation/task2.py
@@ -0,0 +1,13 @@
+###  1. Use `\n` to add another line of "Hello world".
+
+print("Hello world!\nHello world!\nHello world!")
+
+###  2. Add a space between the strings
+
+print("Hello " + "Harshini")
+
+print("Hello" + " Harshini")
+
+print("Hello" + " " + " Harshini")
+
+
